d7ca3a33eed5bd06691602df7080e26086e6318e
CaioGotardo <caiogotardo.dev@gmail.com>
2025-06-18 09:21:17 -0300

    ui

--- SunnyLand/Assets/Scripts/Coletavel.cs
+++ SunnyLand/Assets/Scripts/Coletavel.cs
@@ -2,21 +2,33 @@ using JetBrains.Annotations;
 using System.Collections; //using -> import classes 
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine; // sem ele não roda 
+
 
 public class Coletavel : MonoBehaviour 
 {
-    private int pontos = 5; 
 
-    void Start()
-    {
-        // concatenação cs = concatenação js 
-        Debug.Log("O Jogo começou " + pontos  ); //debug.log = console.log | ; -> OBRIGATORIO
-    }
+public TMP_Text texto_pontos;
+    int pontos = 0;
+    
 
-    void Update()
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        //  Debug.Log("Rolando");
+        if(collision.CompareTag("Player"))
+        {
+
+            pontos++;
+            texto_pontos.text = "Pontos:" + pontos.ToString();
+            collision.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f); 
+                
          
+        }
+
+        
+       
     }
 }
